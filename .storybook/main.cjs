2ad4ea755484d21618695503e91674011a60d92f
tayane <tayperess@hotmail.com>
2022-10-18 16:21:36 -0300

    ajuste

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType}) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/Design-system-react/'
+      config.base = '/Design-System-react/'
     }
     return config
   }
